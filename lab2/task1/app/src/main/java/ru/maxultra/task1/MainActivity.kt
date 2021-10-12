84b3b401f3e37e1738d66ec0678a3aaea0c331e5
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-10-12 22:40:06 +0300

    Log statements for lab2, task1

--- lab2/task1/app/src/main/java/ru/maxultra/task1/MainActivity.kt
+++ lab2/task1/app/src/main/java/ru/maxultra/task1/MainActivity.kt
@@ -1,11 +1,26 @@
 package ru.maxultra.task1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+            fun onAnyEvent(source: LifecycleOwner, event: Lifecycle.Event) =
+                Log.d(TAG, event.name)
+        })
+    }
+
+    private companion object {
+        private const val TAG = "MainActivity"
     }
 }
