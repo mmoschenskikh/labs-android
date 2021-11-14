397ead77600852c067c2ed0d1ea9dcc957738233
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-11-14 15:04:37 +0300

    lab3p5 corrected way of using navigation drawer

--- lab3/lab3p5/app/src/main/java/ru/maxultra/lab3p5/MainActivity.kt
+++ lab3/lab3p5/app/src/main/java/ru/maxultra/lab3p5/MainActivity.kt
@@ -1,23 +1,32 @@
 package ru.maxultra.lab3p5
 
 import android.os.Bundle
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import ru.maxultra.lab3p5.base.BaseActivity
 import ru.maxultra.lab3p5.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val conf = AppBarConfiguration(
-            setOf(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment),
-            binding.drawerLayout
-        )
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.drawerNavView.setupWithNavController(navController)
-        binding.toolbar.setupWithNavController(navController, conf)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
